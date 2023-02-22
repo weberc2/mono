00bb60412d65b6dc6cdcb24f3052a61cf5caac85
weberc2 <weberc2@gmail.cmo>
2023-02-21 18:17:47 -0600

    mod/actionsgenerator: default secret values for registries

--- mod/actionsgenerator/cmd/actionsgenerator/registry.go
+++ mod/actionsgenerator/cmd/actionsgenerator/registry.go
@@ -45,26 +45,54 @@ type Registry struct {
 	// UsernameSecret holds the name of the secret to be provided as the
 	// `username` field to the docker/build-push-image action. For ECR registry
 	// types, this should hold the name of a secret containing the access key
-	// ID for the AWS IAM user with permissions to push the image.
+	// ID for the AWS IAM user with permissions to push the image. If this is
+	// omitted, a default secret name will be assumed based on the registry
+	// type:
+	//
+	// * RegistryTypeDocker: DOCKER_USERNAME
+	// * RegistryTypeECR: AWS_ACCESS_KEY_ID
 	UsernameSecret string
 
 	// PasswordSecret holds the name of the secret to be provided as the
 	// `password` field to the docker/build-push-image action. For ECR registry
 	// types, this should hold the name of a secret containing the secret
-	// access key for the AWS IAM user with permissions to push the image.
+	// access key for the AWS IAM user with permissions to push the image. If
+	// this is omitted, a default secret name will be assumed based on the
+	// registry type:
+	//
+	// * RegistryTypeDocker: DOCKER_PASSWORD
+	// * RegistryTypeECR: AWS_SECRET_ACCESS_KEY
 	PasswordSecret string
 }
 
 // Args builds an `Args` which will be dropped into the docker/build-push-image
 // action configuration.
 func (r *Registry) Args() Args {
+	username := r.UsernameSecret
+	password := r.PasswordSecret
+	if username == "" {
+		username = defaultUsernameSecrets[r.Type]
+	}
+	if password == "" {
+		password = defaultPasswordSecrets[r.Type]
+	}
 	args := Args{
-		"username": fmt.Sprintf("${{ secrets.%s }}", r.UsernameSecret),
-		"password": fmt.Sprintf("${{ secrets.%s }}", r.PasswordSecret),
+		"username": fmt.Sprintf("${{ secrets.%s }}", username),
+		"password": fmt.Sprintf("${{ secrets.%s }}", password),
 	}
 	if r.Type == RegistryTypeDocker && r.ID == "" {
 		return args
 	}
 	args["registry"] = r.ID
 	return args
+}
+
+var defaultUsernameSecrets = [...]string{
+	RegistryTypeDocker: "DOCKER_USERNAME",
+	RegistryTypeECR:    "AWS_ACCESS_KEY_ID",
+}
+
+var defaultPasswordSecrets = [...]string{
+	RegistryTypeDocker: "DOCKER_PASSWORD",
+	RegistryTypeECR:    "AWS_SECRET_ACCESS_KEY",
 }
