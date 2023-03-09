e09c478e8ecdc28e52d767a8394f7ac1668d5790
Craig Weber <weberc2@gmail.com>
2023-03-09 16:23:27 -0600

    actionsgenerator: fix compilation errors

--- actionsgenerator/cmd/actionsgenerator/gomoduleworkflow.go
+++ actionsgenerator/cmd/actionsgenerator/gomoduleworkflow.go
@@ -5,37 +5,6 @@ import (
 	"strings"
 )
 
-type GoModuleImage func(module string) *Image
-
-func GoModImage(mainPackage string) GoModuleImage {
-	return func(module string) *Image {
-		return GoImage(module, mainPackage)
-	}
-}
-
-func (image GoModuleImage) SetDockerfile(dockerfile string) GoModuleImage {
-	return func(module string) *Image {
-		return image(module).SetDockerfile(dockerfile)
-	}
-}
-
-func (image GoModuleImage) SetRegistry(registry *Registry) GoModuleImage {
-	return func(module string) *Image {
-		return image(module).SetRegistry(registry)
-	}
-}
-
-func (image GoModuleImage) SetSinglePlatform(platform string) GoModuleImage {
-	return func(module string) *Image {
-		return image(module).SetSinglePlatform(platform)
-	}
-}
-
-type GoModule struct {
-	Name   string
-	Images []GoModuleImage
-}
-
 func GoModuleWorkflow(name string, images ...GoModuleImage) Workflow {
 	out := Workflow{
 		Name: name,
--- actionsgenerator/cmd/actionsgenerator/main.go
+++ actionsgenerator/cmd/actionsgenerator/main.go
@@ -6,38 +6,31 @@ import (
 )
 
 var workflows = []Workflow{
-	(*GoModule).Workflow(&GoModule{
-		Name: "comments",
-		Images: []GoModuleImage{
-			GoModImage("auth"),
-			GoModImage("tokens"),
-			GoModImage("users"),
-			GoModImage("comments"),
-		},
-	}),
-	(*GoModule).Workflow(&GoModule{
-		Name:   "linkcheck",
-		Images: []GoModuleImage{GoModImage("linkcheck")},
-	}),
-	(*GoModule).Workflow(&GoModule{
-		Name: "gobuilder",
-		Images: []GoModuleImage{
-			GoModImage("gobuilder").
-				// Use the Dockerfile in the module directory rather than the
-				// default Go Dockerfile (the gobuilder Dockerfile preserves
-				// the Go toolchain in the final image so it can build other
-				// images).
-				SetDockerfile("gobuilder/Dockerfile").
-				SetRegistry(&Registry{
-					Type:           RegistryTypeECR,
-					ID:             "988080168334.dkr.ecr.us-east-2.amazonaws.com",
-					UsernameSecret: "GOBUILDER_AWS_ACCESS_KEY_ID",
-					PasswordSecret: "GOBUILDER_AWS_SECRET_ACCESS_KEY",
-				}).
-				// disable multiarch for lambda
-				SetSinglePlatform("linux/amd64"),
-		},
-	}),
+	GoModuleWorkflow(
+		"comments",
+		GoModImage("auth"),
+		GoModImage("tokens"),
+		GoModImage("users"),
+		GoModImage("comments"),
+	),
+	GoModuleWorkflow("linkcheck", GoModImage("linkcheck")),
+	GoModuleWorkflow(
+		"gobuilder",
+		GoModImage("gobuilder").
+			// Use the Dockerfile in the module directory rather than the
+			// default Go Dockerfile (the gobuilder Dockerfile preserves
+			// the Go toolchain in the final image so it can build other
+			// images).
+			SetDockerfile("gobuilder/Dockerfile").
+			SetRegistry(&Registry{
+				Type:           RegistryTypeECR,
+				ID:             "988080168334.dkr.ecr.us-east-2.amazonaws.com",
+				UsernameSecret: "GOBUILDER_AWS_ACCESS_KEY_ID",
+				PasswordSecret: "GOBUILDER_AWS_SECRET_ACCESS_KEY",
+			}).
+			// disable multiarch for lambda
+			SetSinglePlatform("linux/amd64"),
+	),
 	ImageWorkflow(&Image{
 		Name:       "pgbackup",
 		Dockerfile: "./docker/pgbackup/Dockerfile",
