c424980f8b28e0c8be4759df2d4b8cf0bc860490
weberc2 <weberc2@gmail.com>
2023-06-19 12:30:09 -0500

    c-static-site-generator: remove private functions from test.h

--- c-static-site-generator/tests/test.h
+++ c-static-site-generator/tests/test.h
@@ -7,10 +7,8 @@ typedef void (*defer_func)(void *);
 
 #define TEST_DEFER(fn, data) test_defer((defer_func)(fn), (data));
 
-void test_module_init();
 void test_init(char *name);
 void test_defer(defer_func fn, void *data);
-void test_run_defer();
 bool test_fail(const char *format, ...);
 bool test_success();
 
