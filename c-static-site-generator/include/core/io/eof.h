bd2c0b4c48301691133a8399a5ffc9002d95a3a3
weberc2 <weberc2@gmail.com>
2023-06-25 07:22:57 -0500

    c-static-site-generator: remove unused eof.h/c

--- c-static-site-generator/include/core/io/eof.h
+++
@@ -1,8 +0,0 @@
-#ifndef EOF_H
-#define EOF_H
-
-#include "core/error/error.h"
-
-const error eof;
-
-#endif // EOF_H
--- c-static-site-generator/src/core/io/eof.c
+++
@@ -1,6 +0,0 @@
-#include "core/io/eof.h"
-
-static void __attribute__((constructor)) init()
-{
-    error_const((error *)(&eof), "end of file");
-}
