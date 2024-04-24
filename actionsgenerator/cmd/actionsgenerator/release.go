372c7213d20b87f56c075374261754d7ad86f0ab
Craig Weber <weberc2@gmail.com>
2024-04-24 14:30:16 +0200

    actionsgenerator: restore caching

--- actionsgenerator/cmd/actionsgenerator/release.go
+++ actionsgenerator/cmd/actionsgenerator/release.go
@@ -41,6 +41,8 @@ func JobRelease(image *Image) Job {
 			"%[1]s:${{ github.sha }}\n%[1]s:latest",
 			image.FullName(),
 		),
+		"cache-from": "type=gha, scope=${{ github.workflow }}",
+		"cache-to":   "type=gha, scope=${{ github.workflow }}",
 	}
 	if image.SinglePlatform != "" {
 		buildArgs["platforms"] = image.SinglePlatform
