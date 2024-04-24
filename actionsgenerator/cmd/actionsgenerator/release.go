4d142ad70bd2f016a471745f3a47c8bcd9614bcb
Craig Weber <weberc2@gmail.com>
2024-04-24 14:13:35 +0200

    actionsgenerator: update to the new cleaner way to build images

--- actionsgenerator/cmd/actionsgenerator/release.go
+++ actionsgenerator/cmd/actionsgenerator/release.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"strings"
-	"text/template"
 )
 
 // WorkflowRelease builds a job for releasing container images.
@@ -27,7 +26,6 @@ func WorkflowRelease(images ...*Image) Workflow {
 // JobRelease builds a job for releasing container images.
 func JobRelease(image *Image) Job {
 	buildArgs := Args{
-		"builder": "${{ steps.buildx.outputs.name }}",
 		"build-args": func() string {
 			lines := make([]string, 0, len(image.Args))
 			for key, value := range image.Args {
@@ -35,13 +33,15 @@ func JobRelease(image *Image) Job {
 			}
 			return strings.Join(lines, "\n")
 		}(),
-		"context":    image.Context,
-		"file":       image.Dockerfile,
-		"platforms":  "linux/amd64,linux/arm64",
-		"push":       true,
-		"tags":       "${{ steps.prep.outputs.tags }}",
-		"cache-from": "type=gha, scope=${{ github.workflow }}",
-		"cache-to":   "type=gha, scope=${{ github.workflow }}",
+		"context":   image.Context,
+		"file":      image.Dockerfile,
+		"platforms": "linux/amd64,linux/arm64",
+		"push":      true,
+		"tags": fmt.Sprintf(
+			"${{ secrets.DOCKER_USERNAME }}/%[1]s:${{ github.sha }}\n"+
+				"${{ secrets.DOCKER_USERNAME }}/%[1]s:latest",
+			image.Name,
+		),
 	}
 	if image.SinglePlatform != "" {
 		buildArgs["platforms"] = image.SinglePlatform
@@ -50,68 +50,20 @@ func JobRelease(image *Image) Job {
 	return Job{
 		RunsOn: "ubuntu-latest",
 		Steps: []Step{{
-			Name: "Checkout",
-			Uses: "actions/checkout@v3",
-		}, {
-			Name: "Prepare",
-			ID:   "prep",
-			Run:  tmpl(image),
-		}, {
 			Name: "Set up QEMU",
-			Uses: "docker/setup-qemu-action@master",
-			With: Args{"platforms": "all"},
+			Uses: "docker/setup-qemu-action@v3",
 		}, {
 			Name: "Set up Docker Buildx",
-			ID:   "buildx",
-			Uses: "docker/setup-buildx-action@master",
+			Uses: "docker/setup-buildx-action@v3",
 		}, {
 			Name: "Login to DockerHub",
 			If:   "github.event_name != 'pull_request'",
-			Uses: "docker/login-action@v2",
+			Uses: "docker/login-action@v3",
 			With: image.Registry.Args(),
 		}, {
-			Name: "Build",
-			Uses: "docker/build-push-action@v4",
+			Name: "Build and push",
+			Uses: "docker/build-push-action@v5",
 			With: buildArgs,
 		}},
 	}
-}
-
-var releaseJobPrepareStepScriptTemplate = template.Must(
-	template.
-		New("").
-		// use different delims so GH's ${{ secrets.XYZ }} syntax doesn't
-		// collide
-		Delims("{%", "%}").
-		Parse(
-			`DOCKER_IMAGE={% .FullName %}
-VERSION=latest
-SHORTREF=${GITHUB_SHA::8}
-
-# If this is a git tag, use the tag name as a docker tag
-if [[ $GITHUB_REF == refs/tags/* ]]; then
-  VERSION=${GITHUB_REF#refs/tags/v}
-fi
-TAGS="${DOCKER_IMAGE}:${VERSION},${DOCKER_IMAGE}:${SHORTREF}"
-
-# If the VERSION looks like a version number, assume that
-# this is the most recent version of the image and also
-# tag it 'latest'.
-if [[ $VERSION =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-  TAGS="$TAGS,${DOCKER_IMAGE}:latest"
-fi
-
-# Set output parameters.
-echo ::set-output name=tags::${TAGS}
-echo ::set-output name=docker_image::${DOCKER_IMAGE}`))
-
-func tmpl(image *Image) string {
-	var sb strings.Builder
-	if err := releaseJobPrepareStepScriptTemplate.Execute(
-		&sb,
-		image,
-	); err != nil {
-		panic(err)
-	}
-	return sb.String()
 }
